fdee115f797a146ba3fc3179a4954b4e29afcff7
Jeff Chung <chungchi300@hotmail.com>
2018-02-07 14:51:18 +0800

    scope

--- test-for-you-don't-know-js/scope & closures/chapter-3-functional-scope.js
+++ test-for-you-don't-know-js/scope & closures/chapter-3-functional-scope.js
@@ -103,7 +103,7 @@ describe('Lexical scope-Javascript Scope Model', () => {
   });
   it('iife do not pollute global scope', () => {
     var a = 42;
-
+    //if function has (,it is and function expression,not function declaration
     (function IIFE() {
       a = 10;
       expect(a).toBe(10);
@@ -115,12 +115,176 @@ describe('Lexical scope-Javascript Scope Model', () => {
       expect(e instanceof ReferenceError).toBe(true);
     }
   });
-  it('iife global', () => {});
-  it('anonymous function scope', () => {});
-  it('block scope ,let', () => {});
-  it('block scope ,const', () => {});
-  it('try catch scope', () => {});
-  it('garbage collection', () => {});
+  it('iife with params', () => {
+    var a = 42;
+    //if function has (,it is and function expression,not function declaration
+    (function IIFE(name) {
+      a = 10;
+      expect(a).toBe(10);
+      expect(name).toBe('jeffchung');
+    })('jeffchung');
+    expect(a).toBe(10);
+    try {
+      console.log(IIFE);
+    } catch (e) {
+      expect(e instanceof ReferenceError).toBe(true);
+    }
+  });
+  it('iife scope  ', () => {
+    var a = 42;
+    //if function has (,it is and function expression,not function declaration
+    (function IIFE() {
+      //Global of IIFE is global
+      expect(a).toBe(42);
+    })();
+
+    try {
+      console.log(IIFE);
+    } catch (e) {
+      expect(e instanceof ReferenceError).toBe(true);
+    }
+  });
+  it('iife access global in prettier way ', () => {
+    global.a = 42;
+    //if function has (,it is and function expression,not function declaration
+    (function IIFE(global) {
+      //Global of IIFE is global
+      expect(global.a).toBe(42);
+    })(global);
+
+    try {
+      console.log(IIFE);
+    } catch (e) {
+      expect(e instanceof ReferenceError).toBe(true);
+    }
+  });
+  it('iife ensure the undefined safe by param ', () => {
+    /*
+    //Javascript it is read only in safe mode,window
+    undefined = true; // setting a land-mine for other code! avoid!
+
+    (function IIFE(undefined) {
+      var a;
+      expect(a).toBe(undefined);
+    })();
+    */
+  });
+  it('iife invert the execution order of function(this pattern used in UMD module )', () => {
+    global.a = 2;
+
+    (function IIFE(def) {
+      def(global);
+    })(function def(global) {
+      //var hosting
+      expect(a).toBe(undefined);
+      //don't effect global scope too
+      expect(global.a).toBe(2);
+      var a = 3;
+      expect(a).toBe(3);
+    });
+  });
+  it('anonymous function scope,also do not pollute global scope', () => {
+    setTimeout(function() {
+      console.log('I waited 1 second!');
+    }, 1000);
+    /*
+      1. No name in stack trace for debug
+      2. Cannot refernce itself ,it need to use deprecated arguments.callee to do recursion
+      3. Missing the name reduce the readability of code
+    */
+  });
+  it('block scope ,let belongs to nearest block{} scope', () => {
+    function func1() {
+      var foo = 2;
+
+      if (foo) {
+        let bar = foo * 2;
+      }
+      try {
+        console.log(bar); // ReferenceError
+        expect(true).toBe(false);
+      } catch (err) {
+        expect(err instanceof ReferenceError).toBe(true);
+      }
+    }
+    func1();
+    // console.log(bar); // ReferenceError
+  });
+  it('block scope ,let belongs to nearest block{} scope,it can be function or global too', () => {
+    function func1() {
+      var foo = 2;
+      let bar;
+      if (foo) {
+        bar = foo * 2;
+      }
+
+      expect(bar).toBe(4);
+    }
+    func1();
+  });
+  it('block scope ,const', () => {
+    function func1() {
+      var foo = 2;
+
+      if (foo) {
+        const bar = foo * 2;
+      }
+      try {
+        console.log(bar); // ReferenceError
+        expect(true).toBe(false);
+      } catch (err) {
+        expect(err instanceof ReferenceError).toBe(true);
+      }
+    }
+    func1();
+  });
+  it('try catch scope', () => {
+    try {
+      undefined(); // illegal operation to force an exception!
+    } catch (err) {
+      expect(err instanceof Error).toBe(true);
+    }
+
+    //console.log( err ); // ReferenceError: `err` not found
+  });
+  it('garbage collection when function execute done ', () => {
+    /*
+    function process(data) {
+    	// do something interesting
+    }
+
+    var someReallyBigData = { .. };
+
+    process( someReallyBigData );
+
+    var btn = document.getElementById( "my_button" );
+
+    btn.addEventListener( "click", function click(evt){
+    	console.log("button clicked");
+    }, false );
+    //no garbage collection on someReallyBigData because the click function still have the reference
+    */
+    /*
+
+    function process(data) {
+    	// do something interesting
+    }
+
+    // anything declared inside this block can go away after!
+    {
+    	let/var someReallyBigData = { .. };
+
+    	process( someReallyBigData );
+    }
+
+    var btn = document.getElementById( "my_button" );
+
+    btn.addEventListener( "click", function click(evt){
+    	console.log("button clicked");
+    }, false);
+    //have garbage collection on someReallyBigData
+    */
+  });
   it('function writing not only have the var,it also defined', () => {
     try {
       expect(foo(2)).toBe(4);
